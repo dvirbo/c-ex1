924c39d0e67f902c2169def16d71e13d0318289c
dvirbo <dvir.borochov@gmail.com>
2021-11-07 13:21:15 +0200

    last change
    final

--- main.c
+++ main.c
@@ -1,29 +1,12 @@
 #include <stdio.h>
 #include "NumClass.h"
-# define True 1
+#define True 1
 
 // Author: Dvirb
 
-/////////////***////////////////
-void print_prime(int min, int max)
-{
-  printf("\n");
-  printf("The Prime numbers are:");
-  int print_ans;
-  
-  while (min <= max)
-  {
-    print_ans = isPrime(min);
-    if (print_ans == True) // mean that the ans is True
-      printf(" %d", min);
-    min++;
-  }
-}
-
-/////////////***////////////////
 void print_amstrong(int min, int max)
 {
-   printf("The Armstrong numbers are:");
+  printf("The Armstrong numbers are:");
   int print_ans;
   while (min <= max)
   {
@@ -34,21 +17,6 @@ void print_amstrong(int min, int max)
   }
 }
 
-/////////////***////////////////
-void print_strong(int min, int max)
-{
-  printf("\nThe Strong numbers are:");
-  int print_ans;
-  while (min <= max)
-  {
-    print_ans = isStrong(min);
-    if (print_ans == True) // mean that the ans is True
-      printf(" %d", min);
-    min++;
-  }
-}
-
-/////////////***////////////////
 void print_Palindrome(int min, int max)
 {
   printf("\nThe Palindromes are:");
@@ -62,26 +30,56 @@ void print_Palindrome(int min, int max)
   }
 }
 
-void print_func(int min, int max)
-{
-  print_amstrong(min, max);
-  print_Palindrome(min, max);
-  print_prime(min, max);
-  print_strong(min, max);
-}
+  void print_prime(int min, int max)
+  {
+    printf("\nThe Prime numbers are:");
+    int print_ans;
 
-//main function :
+    while (min <= max)
+    {
+      print_ans = isPrime(min);
+      if (print_ans == True) // mean that the ans is True
+        printf(" %d", min);
+      min++;
+    }
+  }
 
-int main()
-{
-  int x, y;
-  scanf("%d%d", &x, &y);
+  void print_strong(int min, int max)
+  {
+    printf("\nThe Strong numbers are:");
+    int print_ans;
+    while (min <= max)
+    {
+      print_ans = isStrong(min);
+      if (print_ans == True) // mean that the ans is True
+        printf(" %d", min);
+      min++;
+    }
+  }
 
-  if (x > y)
-    print_func(y, x);
-  else
-    print_func(x, y);
-    
-        printf("\n");
-  return 0;
-}
+  // print the function by the order that Required.
+  void print_func(int min, int max)
+  {
+    print_amstrong(min, max);
+    print_Palindrome(min, max);
+    print_prime(min, max);
+    print_strong(min, max);
+  }
+
+  // main function :
+  int main()
+  {
+    int x, y;
+    scanf("%d%d", &x, &y);
+
+    if (x > y)
+    {
+      print_func(y, x);
+    }
+    else
+    {
+      print_func(x, y);
+    }
+    printf("\n");
+    return 0;
+  }
